d187d71a022390606fd481f7966660f268b982d3
Pierce <ppuckette@gmail.com>
2022-06-17 12:27:17 -0400

    Review Card setup

--- setup/src/App.js
+++ setup/src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Review from "./Review";
-import { FaGithubSquare } from "react-icons/fa";
+
 function App() {
   return (
-    <h2>
-      reviews project setup
-      <FaGithubSquare className="icon" />
-    </h2>
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline">
+            
+          </div>
+        </div>
+        <Review />
+      </section>
+
+    </main>
   );
 }
 
